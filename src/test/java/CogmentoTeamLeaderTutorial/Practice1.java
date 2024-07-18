5ec90c82135d188b430f5dfe46e0205aa0c60ffc
HP <prafulp1030@gmail.com>
2024-07-18 14:49:09 +0530

    cogmento application testcase is created

---
+++ src/test/java/CogmentoTeamLeaderTutorial/Practice1.java
@@ -0,0 +1,12 @@
+package CogmentoTeamLeaderTutorial;
+
+public class Practice1 {
+
+	public static void main(String[] args) {
+
+
+		System.out.println("Cogmento test Application test case created by TeamLeader");
+
+	}
+
+}
